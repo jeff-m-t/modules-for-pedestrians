1cfd9f71d10ea5a04156364049b7d5f68af46615
Jeff Thompson <jeff.m.t@gmail.com>
2016-02-08 09:54:02 -0800

    fixing integration test

--- src/it/scala/pedestrian/modules/kv/mongodb/MongoDbKVStoreSupportTest.scala
+++ src/it/scala/pedestrian/modules/kv/mongodb/MongoDbKVStoreSupportTest.scala
@@ -2,11 +2,20 @@ package pedestrian.modules.kv.mongodb
 
 import pedestrian.core.BlankSlate
 import pedestrian.modules.kv.KVStoreSupportCommonTests
+import org.scalatest.BeforeAndAfterAll
+import org.mongodb.scala.MongoClient
 
-class MongoDbKVStoreSupportTest extends KVStoreSupportCommonTests {
-  def createModule = new BlankSlate with MongoDbKVStoreSupport {
-    override val mongoUrl = "mongodb://192.168.99.100"
-    override val mongoDatabase = "foo"
-    override val mongoCollection = "kvstore"
+class MongoDbKVStoreSupportTest extends KVStoreSupportCommonTests with BeforeAndAfterAll {
+  type ImplType = MongoDbKVStoreSupport
+  
+  val client = MongoClient("mongodb://192.168.99.100")
+  val collection = client.getDatabase("test").getCollection("kvstore")
+  
+  val createEnvironment: ImplType#KeyValueStoreEnv = collection
+  
+  def createModule = new BlankSlate with MongoDbKVStoreSupport
+  
+  override def afterAll() = {
+    client.close()
   }
 }
